349e682a21ad004ed7ee8933adc11cdf8f4b2865
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-08 17:01:59 -0400

    done

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -15,6 +15,7 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
+       self.capacity = capacity
        self.storage = [None] * capacity
 
 
@@ -27,7 +28,7 @@ def hash(string, max):
     hash = 5381
     for x in string:
         hash = (hash * 33) + ord(x)
-    return hash
+    return hash % max
 
 
 # '''
@@ -36,7 +37,12 @@ def hash(string, max):
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    new_hash = hash(key, hash_table.capacity)
+    print()
+    if  hash_table.storage[new_hash] is None :
+        hash_table.storage[new_hash] = Pair(key,value)
+    elif hash_table.storage[new_hash].key != key:
+        print('Warning key mismatch') 
 
 
 # '''
@@ -45,7 +51,11 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+    _hash = hash(key, hash_table.capacity)
+    if hash_table.storage[_hash] is not None:
+        hash_table.storage[_hash] = None
+    else:
+        print('Does not exist')
 
 
 # '''
@@ -54,7 +64,11 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    _hash = hash(key, hash_table.capacity)
+    if hash_table.storage[_hash] is not None:
+        return hash_table.storage[_hash].value
+    else:
+        return None 
 
 
 def Testing():
